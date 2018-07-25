09e2c52d1a270a47223af143ff91e02b5eea2bbb
Kumarz <2contactsathish@gmail.com>
2018-07-25 14:01:19 -0400

    Added finding common elements in an Array & Kth smallest Element in BST

---
+++ src/kumar/problem/solving/exercises/BalanceBrackets.java
@@ -0,0 +1,25 @@
+package kumar.problem.solving.exercises;
+
+/**
+ * 
+ * @author Kumar
+ * Balance the brackets in such a way that there should be equal number of open brackets should be followed by equal nbumber of closed brackets.
+ *
+ * {{{}{{}  => {{}}
+ * Follow up: make it better by remove any characters other than brackets, if given.
+ * 
+ */
+
+public class BalanceBrackets {
+	static String OPEN_BRACKET_STR = "{";
+	static String CLOSE_BRACKET_STR = "}";
+	static int OPEN_BRACKETS_COUNT = 0;
+	static int CLOSE_BRACKETS_COUNT = 0;
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
