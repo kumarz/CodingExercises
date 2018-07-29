963077e5a4be3abfeb915756bab8d8a602e742dd
Kumarz <2contactsathish@gmail.com>
2018-07-29 17:12:02 -0400

    Added code to resolve balancing brackets in a given string.

--- src/kumar/coding/exercises/problemSolving/String/BalanceBrackets.java
+++ src/kumar/coding/exercises/problemSolving/String/BalanceBrackets.java
@@ -3,7 +3,7 @@ package kumar.coding.exercises.problemSolving.String;
 /**
  * 
  * @author Kumar
- * Balance the brackets in such a way that there should be equal number of open brackets should be followed by equal nbumber of closed brackets.
+ * Balance the brackets in such a way that there should be equal number of open brackets should be followed by equal number of closed brackets.
  *
  * {{{}{{}  => {{}}
  * Follow up: make it better by remove any characters other than brackets, if given.
@@ -11,15 +11,43 @@ package kumar.coding.exercises.problemSolving.String;
  */
 
 public class BalanceBrackets {
-	static String OPEN_BRACKET_STR = "{";
-	static String CLOSE_BRACKET_STR = "}";
+	static char OPEN_BRACKET_STR = '{';
+	static char CLOSE_BRACKET_STR = '}';
 	static int OPEN_BRACKETS_COUNT = 0;
 	static int CLOSE_BRACKETS_COUNT = 0;
+	static int TOTAL_OPEN_BRACKETS_COUNT = 0;
+	static int TOTAL_CLOSE_BRACKETS_COUNT = 0;
 	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String input = "{{{}{{}";
+		StringBuffer output = new StringBuffer();
+		
+		// to count the brackers
+		for(char item: input.toCharArray()){
+			if(item == OPEN_BRACKET_STR){
+				TOTAL_OPEN_BRACKETS_COUNT++;
+			}else if(item == CLOSE_BRACKET_STR){
+				TOTAL_CLOSE_BRACKETS_COUNT++;
+			}
+		}
+		
+		for(int count = 0; count < input.length(); count++){
+			char current = input.charAt(count);
+			if(current == OPEN_BRACKET_STR){
+				OPEN_BRACKETS_COUNT++;
+			}
+			if(current == CLOSE_BRACKET_STR){
+				CLOSE_BRACKETS_COUNT++;
+			}
+			if(current == OPEN_BRACKET_STR && OPEN_BRACKETS_COUNT <= TOTAL_CLOSE_BRACKETS_COUNT){
+				output.append(current);
+			}
+			if(current == CLOSE_BRACKET_STR && OPEN_BRACKETS_COUNT <= TOTAL_OPEN_BRACKETS_COUNT){
+				output.append(current);
+			}
+		}
+		System.out.println(output.toString());
 	}
 
 }
