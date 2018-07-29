db91b9230d1fe0de2efa2c013ac9bbfc7656beb9
Kumarz <2contactsathish@gmail.com>
2018-07-29 12:18:31 -0400

    Added code to check for first Unique character for a given string.

--- src/kumar/problem/solving/exercises/FindShortestRangeInaSortedIntegerArray.java
+++ src/kumar/problem/solving/exercises/FindShortestRangeInaSortedIntegerArray.java
@@ -23,7 +23,6 @@ public class FindShortestRangeInaSortedIntegerArray {
 	int[] a = new int[] {4,7,9,12,15};
 	int[] b = new int[] {0,8,10,14,20};
 	int[] c = new int[] {6,12,16,30,50};
-	
 	public static void main(String[] args) {
 
 	}
