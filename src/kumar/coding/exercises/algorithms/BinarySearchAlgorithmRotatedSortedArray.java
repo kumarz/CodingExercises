a41e435bceb8785d2657e7826638d6930ee77cc3
Kumarz <2contactsathish@gmail.com>
2018-08-08 14:58:33 -0400

    Added code for finding an element in a rotated sorted binary search algorithm.

---
+++ src/kumar/coding/exercises/algorithms/BinarySearchAlgorithmRotatedSortedArray.java
@@ -0,0 +1,74 @@
+package kumar.coding.exercises.algorithms;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Kumar
+ * This is an implementation of a Binay Search Algorithm for a roated sorting algorithm.
+ * This has an time complexity of O(logn)
+ *
+ */
+
+public class BinarySearchAlgorithmRotatedSortedArray {
+	private static int[] sample = {1,3,5,6};
+	public static void main (String[]args){
+		System.out.println("Please enter an item to be Searched: ");
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		int output = binarySearch(input);
+		if(output == -1){
+			System.out.println("Your element is not found !");
+		}else{
+			System.out.println("Your element is found at index: "+ output);
+		}
+		sc.close();
+	}
+	
+	private static int binarySearch(int searchItem){
+
+        int start = 0;
+        int end = sample.length - 1;
+        
+        while(start <= end){
+            int mid = start+(end-start)/2;
+            if(searchItem == sample[mid]){
+                return mid;
+            }
+            if(searchItem < sample[mid]){
+                end = mid - 1;
+            }else {
+                start = mid + 1;
+            }
+        }
+            
+        return -1;
+	}
+	
+	public int binarySearchIterative(int[] nums, int target) {
+	    int left = 0;
+	    int right= nums.length-1;
+	 
+	    while(left<=right){
+	        int mid = left + (right-left)/2;
+	        if(target==nums[mid])
+	            return mid;
+	 
+	        if(nums[left]<=nums[mid]){
+	            if(nums[left]<=target&& target<nums[mid]){
+	                right=mid-1;
+	            }else{
+	                left=mid+1;
+	            }
+	        }else{
+	            if(nums[mid]<target&& target<=nums[right]){
+	                left=mid+1;
+	            }else{
+	                right=mid-1;
+	            }
+	        }    
+	    }
+	 
+	    return -1;
+	}
+}
