16b2f00433f7c419146e103e6637163ee6afefad
Kumarz <2contactsathish@gmail.com>
2018-08-10 01:33:27 -0400

    Added the model class as well.

---
+++ src/kumar/codeing/exercise/model/Pair.java
@@ -0,0 +1,10 @@
+package kumar.codeing.exercise.model;
+
+public class Pair {
+	public int value;
+    public int count;
+    public Pair(int i, int c){
+        this.value  = i;
+        this.count  = c;
+    }
+}
