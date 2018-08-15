dee2a33d2a1c11864996d7595594df18d18e6bf0
Kumarz <2contactsathish@gmail.com>
2018-08-15 13:02:25 -0400

    Added code for doing it in O(1) space.

--- src/kumar/coding/exercises/problemSolving/String/LongestValidParantheses.java
+++ src/kumar/coding/exercises/problemSolving/String/LongestValidParantheses.java
@@ -13,10 +13,11 @@ public class LongestValidParantheses {
 	public static void main(String[] args) {
 		String input ="((()))";
 		System.out.println(approach1(input));
+		System.out.println(approach2(input));
 	}
 
 	/**
-	 * Time complexity is O(n) Space Complexity is O(1). 
+	 * Time complexity is O(n) Space Complexity is O(n) for using Stack of n elements. 
 	 * @param input
 	 * @return
 	 */
@@ -38,5 +39,40 @@ public class LongestValidParantheses {
 		}
 		return max;
 	}
+	
+	/**
+	 * Time complexity is O(n) Space Complexity is O(1) only left & right are needed. 
+	 * @param input
+	 * @return
+	 */
+	public static int approach2(String s) {
+        int left = 0, right = 0, maxlength = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                maxlength = Math.max(maxlength, 2 * right);
+            } else if (right >= left) {
+                left = right = 0;
+            }
+        }
+        left = right = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                maxlength = Math.max(maxlength, 2 * left);
+            } else if (left >= right) {
+                left = right = 0;
+            }
+        }
+        return maxlength;
+    }
 
 }
