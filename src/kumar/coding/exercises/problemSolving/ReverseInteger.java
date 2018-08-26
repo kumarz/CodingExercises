910f37e1bea1ae0aa6837e9599a7ae6183799ce6
Kumarz <2contactsathish@gmail.com>
2018-08-26 10:11:16 -0400

    Added code to check for reversing an integer.

---
+++ src/kumar/coding/exercises/problemSolving/ReverseInteger.java
@@ -0,0 +1,31 @@
+package kumar.coding.exercises.problemSolving;
+/**
+ * We are checking if the integer is a palindrome.
+ * Time complexity : O(log10(n). We divided the input by 10 for every iteration, so the time complexity is O(log10(n))
+ * Space complexity : O(1).
+ * @author Kumar
+ *
+ */
+
+public class ReverseInteger {
+
+	public static void main(String[] args) {
+		int input = 1001;
+		System.out.println(execute(input));
+
+	}
+	
+	public static boolean execute(int x){
+		if(x <= 0 || x%10 == 0){
+           return false;
+        }
+        
+        int reverseNumber = 0;
+        while(x > reverseNumber){
+            reverseNumber = reverseNumber * 10 + x%10;
+            x = x/10;
+        }
+        return reverseNumber == x || reverseNumber/10 == x;
+	}
+
+}
