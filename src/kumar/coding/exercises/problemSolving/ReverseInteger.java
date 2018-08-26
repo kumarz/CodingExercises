22b7bf888dee369ef871743e284b6d4ca94ced0c
Kumarz <2contactsathish@gmail.com>
2018-08-26 10:17:28 -0400

    Fixed a bug when the input is 0.

--- src/kumar/coding/exercises/problemSolving/ReverseInteger.java
+++ src/kumar/coding/exercises/problemSolving/ReverseInteger.java
@@ -16,7 +16,7 @@ public class ReverseInteger {
 	}
 	
 	public static boolean execute(int x){
-		if(x <= 0 || x%10 == 0){
+		 if(x < 0 || (x%10 == 0 &&  x!=0)){
            return false;
         }
         
