1ddfa4dd7527af6930881fdaf143985f0e946c79
Kumarz <2contactsathish@gmail.com>
2018-10-07 16:56:43 -0400

    Added another method for approach2

--- src/kumar/coding/exercises/problemSolving/Arrays/PlusOne.java
+++ src/kumar/coding/exercises/problemSolving/Arrays/PlusOne.java
@@ -9,7 +9,8 @@ package kumar.coding.exercises.problemSolving.Arrays;
 public class PlusOne {
 
 	public static void main(String[] args) {
-		int[] output = execute();
+		// int[] output = execute();
+		int[] output = execute2();
 		for(int item: output){
 			System.out.println(item);	
 		}
@@ -18,6 +19,8 @@ public class PlusOne {
 
 	/**
 	 * calculated using carry.
+	 * Time complexity is O(n)
+	 * Space complexity is O(1)
 	 * @return
 	 */
 	private static int[] execute() {
@@ -38,6 +41,28 @@ public class PlusOne {
 	        for(int i = 1; i < digits.length; i++){
 	            result[i] = digits[i-1];
 	        }
+	        
+	        return result;
+	}
+	
+
+	/**
+	 * calculated using just array.
+	 * @return
+	 */
+	private static int[] execute2() {
+		int[] digits = {1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9};
+	        
+	        for(int i = digits.length -1; i >=0; i--){
+	            if(digits[i] < 9){
+	            	digits[i]++;
+	            	return digits;
+	            }
+	            digits[i] = 0;
+	        }
+	        
+	        int[] result = new int[digits.length+1];
+	        result[0] = 1;
 	        
 	        return result;
 	}
