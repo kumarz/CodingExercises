bc423055eceafbb113a1dc64c99fac5e2912fa82
Kumarz <2contactsathish@gmail.com>
2018-12-06 23:30:44 -0500

    Added code for distinct K characters of substring.

--- src/kumar/coding/exercises/problemSolving/DistinctKCharacterSubstring.java
+++ src/kumar/coding/exercises/problemSolving/DistinctKCharacterSubstring.java
@@ -10,21 +10,20 @@ public class DistinctKCharacterSubstring {
 	public static void main(String[] args) {
 		String input = "kjabjfnljaghjkkleuroitybnmbxznawlqhejwheqw";
 		List<String> output = new ArrayList<String>();
-		int k = 2;
+		int k = 5;
 		Map<Character, Integer> lookup = new HashMap<Character, Integer>();
 		for(int i = 0; i+k < input.length(); i++){
 			String item = input.substring(i, i+k);
-			boolean hasDuplicates = false;
+			int duplicateCount = 0;
 			lookup.clear();
 			for(char temp : item.toCharArray()){
 				lookup.put(temp, lookup.getOrDefault(temp, 0)+1);
 				if(lookup.getOrDefault(temp, 0) > 1){
-					hasDuplicates = true;
-					continue;
+					duplicateCount++;
 				}
 			}
 			
-			if(!hasDuplicates){
+			if(duplicateCount == 1){
 				output.add(item);
 				i += k;
 			}
