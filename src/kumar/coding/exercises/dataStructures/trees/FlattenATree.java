457db4ade2350cc3b365e925d43b346efb4b54bc
Kumarz <2contactsathish@gmail.com>
2018-12-28 21:05:46 -0500

    Fixed a bug for flattening the tree to a list.

--- src/kumar/coding/exercises/dataStructures/trees/FlattenATree.java
+++ src/kumar/coding/exercises/dataStructures/trees/FlattenATree.java
@@ -5,17 +5,12 @@ import kumar.codeing.exercise.model.TreeNode;
 public class FlattenATree {
 	static TreeNode prev = null;
 	public static void main(String[] args) {
-		TreeNode head = new TreeNode(2);
-		head.left = new TreeNode(1);
-		head.left.left = new TreeNode(0);
-		head.left.left.left = new TreeNode(-1);
+		TreeNode head = new TreeNode(1);
+		head.left = new TreeNode(2);
+		head.left.left = new TreeNode(3);
 		head.left.right = new TreeNode(4);
-		head.left.right.left = new TreeNode(-3);
-		head.left.right.right = new TreeNode(8);
-		head.right = new TreeNode(3);
-		head.right.left = new TreeNode(-2);
-		head.right.right = new TreeNode(5);
-		head.right.right.right = new TreeNode(6);
+		head.right = new TreeNode(5);
+		head.right.right = new TreeNode(6);
 		
 		preOrderTraversal(head);
 		System.out.println(" FYI..............ends here");
@@ -28,8 +23,8 @@ public class FlattenATree {
 			return ;
 		}
 		
-		flatten(root.left);
 		flatten(root.right);
+		flatten(root.left);
 		
 		root.left = null;
 		root.right = prev;
@@ -38,28 +33,14 @@ public class FlattenATree {
 	}
 	
 	public static void preOrderTraversal(TreeNode root){
-		if(root != null){
+		if(root == null) {
+			System.out.println("Null");	
+		}else{
 			System.out.println(root.val);
-			if(root.left != null) preOrderTraversal(root.left);
-			if(root.right != null) preOrderTraversal(root.right);
+			preOrderTraversal(root.left);
+			preOrderTraversal(root.right);
 		}
+		
 	}
 	
-	public static void inOrderTraversal(TreeNode root){
-		if(root != null){
-			if(root.left != null) inOrderTraversal(root.left);
-			System.out.println(root.val);
-			if(root.right != null) inOrderTraversal(root.right);
-		}
-	}
-	
-	public static void treeTraversal(TreeNode root){
-		if(root != null){
-			System.out.println(root.val);
-			if(root.left != null) treeTraversal(root.left);
-			if(root.right != null) treeTraversal(root.right);
-		}
-	}
-	
-
 }
